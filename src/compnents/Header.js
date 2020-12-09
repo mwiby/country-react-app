bae4fea90dc51b57b05c74b43ea5464072ebb994
mwiby <markuswb77@gmail.com>
2020-12-09 18:13:33 +0100

    Css onClick circle globe

--- src/compnents/Header.js
+++ src/compnents/Header.js
@@ -1,17 +1,23 @@
 import React from "react";
+import { useState } from "react";
 
 const Header = (props) => {
   const { getData, loading } = props;
+  const [circle, setCircle] = useState([true]);
 
   const handleClick = () => {
     loading(true);
+    setCircle(false);
     getData();
   };
   return (
     <>
       <header className="title">
         <h1>World Countries</h1>
-        <div className="circle" onClick={handleClick}></div>
+        <div
+          className={circle ? "circle-active" : "circle-inactive"}
+          onClick={handleClick}
+        ></div>
       </header>
     </>
   );
