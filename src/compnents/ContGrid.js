e13fab52ad417fc2f16002c6d6311e6e2e9f8380
mwiby <markuswb77@gmail.com>
2020-12-16 14:45:05 +0100

    country card css added

--- src/compnents/ContGrid.js
+++ src/compnents/ContGrid.js
@@ -3,12 +3,10 @@ import Country from "./Country";
 
 const ContGrid = ({ countries }) => {
   return (
-    <div className="cont-grid">
-      <div className="cards">
-        {countries.map((country, i) => (
-          <Country key={i} country={country} />
-        ))}
-      </div>
+    <div className="cards">
+      {countries.map((country, i) => (
+        <Country key={i} country={country} />
+      ))}
     </div>
   );
 };
